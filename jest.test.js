37154c8000a825277f1b681c0dc53e755251c2a7
HunterHaller <huntinghollers@proton.me>
2026-02-20 14:39:06 +0100

    Removed example tests from tutorials

--- jest.test.js
+++ jest.test.js
@@ -1,14 +1,3 @@
-// EXAMPLE TEST:
-//const sum = require('./sum');
-
-//Replace 'require' with import/export syntax, as allowed by babel:
-import { sum } from "./sum.js";
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
-
-
 // PROJECT TESTS:
 
 //capitalize
--- sum.js
+++
@@ -1,6 +0,0 @@
-//ESM export, rather than use module.export
-export function sum(a, b) {
-  return a + b;
-}
-
-//module.exports = sum;
